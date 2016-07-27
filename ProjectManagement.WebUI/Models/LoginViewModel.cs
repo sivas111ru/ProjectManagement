660b50aab1f13027ae77a2066e2e82726efc607d
Pereskokov Vlad @CFU <pereskokow@gmail.com>
2016-07-27 15:17:22 +0300

    simple bootstrap login form

--- ProjectManagement.WebUI/Models/LoginViewModel.cs
+++ ProjectManagement.WebUI/Models/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,11 @@ namespace ProjectManagement.WebUI.Models
     public class LoginViewModel
     {
         [Required]
+        [DisplayName("Почтовый адресс")]
         public string Email { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        [DisplayName("Пароль")]
         public string Password { get; set; }
     }
 }

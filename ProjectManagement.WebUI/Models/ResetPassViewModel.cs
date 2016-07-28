b40530553968d9279fbecc78d06d4d406fd0cea9
Pereskokov Vlad @CFU <pereskokow@gmail.com>
2016-07-28 10:06:21 +0300

    Updated styles of ResetPass.cshtml

--- ProjectManagement.WebUI/Models/ResetPassViewModel.cs
+++ ProjectManagement.WebUI/Models/ResetPassViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,7 @@ namespace ProjectManagement.WebUI.Models
     public class ResetPassViewModel
     {
         [Required]
+        [DisplayName("Почтовый адресс")]
         public string Email { get; set; }
     }
 }

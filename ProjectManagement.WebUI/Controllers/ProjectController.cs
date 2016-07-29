a66947bad7aa892db9a586a789fa72bbf57bae03
sivas111 <sivas111@ya.ru>
2016-07-29 13:39:32 +0300

    remove ViewProjects from ProjectController

--- ProjectManagement.WebUI/Controllers/ProjectController.cs
+++ ProjectManagement.WebUI/Controllers/ProjectController.cs
@@ -19,31 +19,5 @@ namespace ProjectManagement.WebUI.Controllers
             repository = data;
         }
 
-        public ViewResult ViewProjects()
-        {
-            List<ProjectsViewModel> projectsToShow = new List<ProjectsViewModel>();
-
-            var result = repository.Projects.OrderByDescending(d => d.createDate)
-                    .Take(5)
-                    .Join(repository.Users, p => p.fkInitiator, u => u.id, (p, u) => new
-                    {
-                        id = p.id,
-                        name = p.name,
-                        initiator = u.name,
-                        createDate = p.createDate
-                    }).ToList();
-
-            foreach (var project in result)
-            {
-                projectsToShow.Add(new ProjectsViewModel
-                {
-                    id = project.id,
-                    name = project.name,
-                    createDate = project.createDate,
-                    Initiator = project.initiator
-                });
-            }
-            return View(projectsToShow);
-        }
     }
 }

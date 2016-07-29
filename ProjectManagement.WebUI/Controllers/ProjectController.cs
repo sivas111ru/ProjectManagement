99384ea014c8cb1e377fd8e1643c63e94c8d09ec
sivas111 <sivas111@ya.ru>
2016-07-29 13:54:10 +0300

    add ProjectPageViewModel + method in ProjectController ProjectPage

--- ProjectManagement.WebUI/Controllers/ProjectController.cs
+++ ProjectManagement.WebUI/Controllers/ProjectController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using Castle.Components.DictionaryAdapter;
 using ProjectManagement.Domain.Abstract;
+using ProjectManagement.Domain.Entities;
 using ProjectManagement.WebUI.Models;
 
 namespace ProjectManagement.WebUI.Controllers
@@ -19,5 +20,20 @@ namespace ProjectManagement.WebUI.Controllers
             repository = data;
         }
 
+
+        public ViewResult ProjectPage(int id)
+        {
+            var result = (from p in repository.Projects where p.id == id   // to optimize
+                           join t in repository.Tasks on p.id equals t.fkProject
+                           join upm in repository.UsersTasksMap on t.id equals upm.fkTask
+                           join u in repository.Users on upm.fkUser equals u.id
+                           select u).ToList();
+ 
+
+
+
+            return View(new ProjectPageViewModel() { UsersInvolved = result, Description = repository.Projects.Where(i => i.id == id).Select(d => d.description).FirstOrDefault() });
+
+        }
     }
 }
---
+++ ProjectManagement.WebUI/Models/ProjectPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using ProjectManagement.Domain.Entities;
+
+namespace ProjectManagement.WebUI.Models
+{
+    public class ProjectPageViewModel
+    {
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public List<User> UsersInvolved { get; set; }
+    }
+}

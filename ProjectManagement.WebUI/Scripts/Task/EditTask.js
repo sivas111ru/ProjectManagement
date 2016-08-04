b206f98b15074a7c58e9a9b59cdfb3612a592d99
Pereskokov Vlad @CFU <pereskokow@gmail.com>
2016-08-04 15:31:32 +0300

    added render section fix, Priorities are colored on page load

---
+++ ProjectManagement.WebUI/Scripts/Task/EditTask.js
@@ -0,0 +1,16 @@
+﻿var classedDropDownListOnChangeListener = function (controller_id) {
+    var $this = $(controller_id);
+
+    $this
+        .removeClass()
+        .addClass("form-control")
+        .addClass($this.find("option:selected").attr("class"));
+}
+
+$("select").change(function() {
+    classedDropDownListOnChangeListener(this);
+});
+
+$("select").each(function(index, item) {
+    classedDropDownListOnChangeListener(item);
+})

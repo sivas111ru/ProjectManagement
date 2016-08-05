339020ae4e52c235bd42cf47137969c010ece498
Pereskokov Vlad @CFU <pereskokow@gmail.com>
2016-08-05 14:14:52 +0300

    added status and priority to TaskViewModel

--- ProjectManagement.WebUI/Models/TaskViewModel.cs
+++ ProjectManagement.WebUI/Models/TaskViewModel.cs
@@ -10,5 +10,8 @@ namespace ProjectManagement.WebUI.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public UserPageViewModel UserViewModel { get; set; }
+        public String Status { get; set; }
+        public byte Priority { get; set; }
+        public string PriorityCssClass { get; set; }
     }
 }

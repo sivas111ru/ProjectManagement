4c528ac1b5bdb78d2f157799e47f7ce95e54e643
Pereskokov Vlad @CFU <pereskokow@gmail.com>
2016-08-08 14:31:37 +0300

    basic user search works in EditTask

---
+++ ProjectManagement.WebUI/Scripts/app/UserSearch/UserSearchModule.js
@@ -0,0 +1,28 @@
+﻿var userSearchCtrl = function ($scope, SearchUsersService) {
+
+    $scope.users = [];
+
+    $scope.inputSearchFieldListener = function () {
+        var search_str = $scope.searchInput;
+
+        if (search_str.length > 2) {
+            SearchUsersService.search(search_str).success(function (data) {
+                $scope.users = data;
+                console.log(data[0]);
+            });
+        }
+    }
+}
+
+var SearchUsersService = function ($http) {
+    return {
+        search: function(str) {
+            return $http.post("/Api/SearchUsers/" + str);
+        }
+    };
+}
+
+angular
+    .module("UserSearch", [])
+    .factory("SearchUsersService", ["$http", SearchUsersService])
+    .controller("userSearchCtrl", ["$scope", "SearchUsersService", userSearchCtrl]);
